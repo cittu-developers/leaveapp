8c15b10f55512e86c788e6da7b6c2e5ea6f35a35
salam081 <99320689+salam081@users.noreply.github.com>
2025-03-04 08:56:49 +0100

    Update settings.py

--- alms/settings.py
+++ alms/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mn*2#5q&16#yhvxl4=14!b6+5ou=w&2t6=xp8-u_wgsm$h1$dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
